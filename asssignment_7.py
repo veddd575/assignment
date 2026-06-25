0d201c95c8390d6add9d7f22e2c067a2eb116d28
veddd575 <vedantwagh847@gmail.com>
2026-06-25 16:07:00 +0530

    Types of variable

--- asssignment_7.py
+++ asssignment_7.py
@@ -5,3 +5,27 @@ print("Numeric data types: ")
 print(type(a))
 print(type(b))
 print(type(c))
+#sequence types
+a="Hello"
+b=[1,2,3,4,"Vedant","aditya"]
+c=(100,200,300)
+print("Sequences Date Types :")
+print(type(a))
+print(type(b))
+print(type(c))
+# Eaxmple of  set data type
+x = 10
+y = 40
+result = x + y
+print("result :",result)
+print("Data type of result :" ,type(result))
+ 
+ # Types of variablee
+  
+name_str= "vedant"
+print("String to list :",list(name_str))
+my_list = [23,24,28]
+print("list to Tuple : ",tuple(my_list))
+call_log="8180961820"
+print(" string to list:",list(str(call_log)))
+
