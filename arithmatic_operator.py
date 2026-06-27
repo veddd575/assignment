12a697a86ab7a0ef5b1f97e28a3abc08d7dd4a23
veddd575 <vedantwagh847@gmail.com>
2026-06-27 09:02:17 +0530

    completed

--- arithmatic_operator.py
+++ arithmatic_operator.py
@@ -1,6 +1,49 @@
-print("Arithmatic Operators")
+print("Arithmatic Operators 1")
 num_1= 80
 num_2= 50
+print("Addition of two numbers :",num_1+num_2)
+print("Subtraction of two numbers :",num_1-num_2)
+print("Multiplication of two numbers :",num_1*num_2)
+print("Division of two numbers :",num_1/num_2)
+print("floor Division of two numbers :",num_1//num_2)
+
+#2
+
+print("Arithmatic Operators 2")
+num_1= 10
+num_2= 2.3
+print("Addition of two numbers :",num_1+num_2)
+print("Subtraction of two numbers :",num_1-num_2)
+print("Multiplication of two numbers :",num_1*num_2)
+print("Division of two numbers :",num_1/num_2)
+print("floor Division of two numbers :",num_1//num_2)
+
+#3
+
+print("Arithmatic Operators 3")
+num_1= -20
+num_2= 100
+print("Addition of two numbers :",num_1+num_2)
+print("Subtraction of two numbers :",num_1-num_2)
+print("Multiplication of two numbers :",num_1*num_2)
+print("Division of two numbers :",num_1/num_2)
+print("floor Division of two numbers :",num_1//num_2)
+
+#4
+
+print("Arithmatic Operators 4")
+num_1= 100
+num_2= 4.56
+print("Addition of two numbers :",num_1+num_2)
+print("Subtraction of two numbers :",num_1-num_2)
+print("Multiplication of two numbers :",num_1*num_2)
+print("Division of two numbers :",num_1/num_2)
+print("floor Division of two numbers :",num_1//num_2)
+
+#5
+print("Arithmatic Operators 5")
+num_1= 56
+num_2= 24.3
 print("Addition of two numbers :",num_1+num_2)
 print("Subtraction of two numbers :",num_1-num_2)
 print("Multiplication of two numbers :",num_1*num_2)
--- comparasion_operator.py
+++ comparasion_operator.py
@@ -1,3 +1,4 @@
+print("comparasion_operator 1")
 num_1 = 32
 num_2 = 90
 num_3 = 32
@@ -9,5 +10,56 @@ print("num_1 >= num_2 :", num_1 >= num_2)
 print("num_1 <= num_3 :", num_1 <= num_3)
 print("num_2 == num_3 :", num_2 == num_3)
 print("num_1 == num_3 :", num_1 == num_3)
+
+
+#2
+print("comparasion_operator 2")
+num_1 = 200
+num_2 = 100
+print("num_1 == num_2 :", num_1 == num_2)
+print("num_1 != num_2 :", num_1 != num_2)
+print("num_1 > num_2 :", num_1 > num_2)
+print("num_1 < num_2 :", num_1 < num_2)
+print("num_1 >= num_2 :", num_1 >= num_2)
+print("num_1 <= num_3 :", num_1 <= num_3)
+
+
+#3
+print("comparasion_operator 3")
+num_1 = 1
+num_2 = 90
+num_3 = 50
+print("num_1 == num_2 :", num_1 == num_2)
+print("num_1 != num_2 :", num_1 != num_2)
+print("num_1 > num_2 :", num_1 > num_2)
+print("num_1 < num_2 :", num_1 < num_2)
+print("num_1 >= num_2 :", num_1 >= num_2)
+print("num_1 <= num_3 :", num_1 <= num_3)
+print("num_2 == num_3 :", num_2 == num_3)
+print("num_1 == num_3 :", num_1 == num_3)
+
+
+#4
+print("comparasion_operator 4")
+num_1 = 500
+num_2 = 4
+print("num_1 == num_2 :", num_1 == num_2)
+print("num_1 != num_2 :", num_1 != num_2)
+print("num_1 > num_2 :", num_1 > num_2)
+print("num_1 < num_2 :", num_1 < num_2)
+print("num_1 >= num_2 :", num_1 >= num_2)
+print("num_1 <= num_3 :", num_1 <= num_3)
+
+
+#5
+print("comparasion_operator 5")
+num_1 = 100
+num_2 = 200
+print("num_1 == num_2 :", num_1 == num_2)
+print("num_1 != num_2 :", num_1 != num_2)
+print("num_1 > num_2 :", num_1 > num_2)
+print("num_1 < num_2 :", num_1 < num_2)
+print("num_1 >= num_2 :", num_1 >= num_2)
+print("num_1 <= num_3 :", num_1 <= num_3)
 
 
