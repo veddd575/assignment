bd949a3c3385557b20c02beeeb5fadea082eefe5
veddd575 <vedantwagh847@gmail.com>
2026-07-01 14:57:30 +0530

    20 example

---
+++ daily_practise.py
@@ -0,0 +1,168 @@
+print("Bitwise Operators")
+#1
+a = 5
+b = 8
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#2
+a = 5
+b = 15
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#3
+a = 12
+b = 16
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#4
+a = 11
+b = 7
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#5
+a = 57
+b = 8
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#6
+a = 1
+b = 2
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#7
+a = 22
+b = 57
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#8
+a = 9
+b = 33
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#9
+a = 20
+b = 35
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#10
+a = 3
+b = 33
+print("Bitwise AND:", a & b)
+print("Bitwise OR:", a | b)
+print("Bitwise XOR:", a ^ b)
+print("Bitwise NOT:", ~a)
+print("Bitwise NOT:", ~b)
+print("Left Shift:", a << 2)
+print("Right Shift:", a >> 2)
+
+#11
+print("----------------")
+a = 12
+b = 52
+print( (a & b) >> (a | b))
+
+
+#12 
+x = 2
+y = 5
+print( (x | y) << (x ^ y))
+
+
+#13
+y = 3
+x = 5
+print((y ^ x) & ( x ^ y))
+
+#14
+a = 5
+b = 3
+print((a&b) | (a ^ b))
+
+#15
+a = 23
+b = 45
+print((a|b) ^ (a << 2))
+
+
+#16
+x = 12
+y = 5
+print((x << 1) & (y >>  1))
+
+
+#17 
+c = 67
+v = 34
+print((c & v) << (c ^ v))
+
+
+# 18
+v = 12 
+print((v << 2) | (v >> 2))
+
+#19
+a = 33
+b = 12
+print((a ^ b) & (a | b))
+
+#20
+f = 22
+g = 19
+print((f | g) & (f ^ g) <<( f >> 1))
+
+
+
